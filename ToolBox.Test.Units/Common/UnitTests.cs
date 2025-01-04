5ca22d766152024e032f57ceee67aaf1fb2ae15a
Zeid Youssefzadeh <zeidlabco@gmail.com>
2025-01-04 12:37:21 +0330

    A unit is supposed to work as Void it is imported from LanguagesExt and modified to fit the use case

---
+++ ToolBox.Test.Units/Common/UnitTests.cs
@@ -0,0 +1,169 @@
+﻿using FluentAssertions;
+using ZeidLab.ToolBox.Common;
+
+namespace ZeidLab.ToolBox.Test.Units.Common;
+
+public class UnitTests
+{
+    [Fact]
+    public void Default_Should_Return_UnitInstance()
+    {
+        // Act
+        Unit unit = Unit.Default;
+
+        // Assert
+        unit.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void ToString_Should_Return_Parentheses()
+    {
+        // Arrange
+        Unit unit = Unit.Default;
+
+        // Act
+        string result = unit.ToString();
+
+        // Assert
+        result.Should().Be("()");
+    }
+
+    [Fact]
+    public void CompareTo_Should_Return_Zero()
+    {
+        // Arrange
+        Unit unit1 = Unit.Default;
+        Unit unit2 = Unit.Default;
+
+        // Act
+        int result = unit1.CompareTo(unit2);
+
+        // Assert
+        result.Should().Be(0);
+    }
+
+    [Fact]
+    public void GreaterThanOperator_Should_Return_False()
+    {
+        // Arrange
+        Unit unit1 = Unit.Default;
+        Unit unit2 = Unit.Default;
+
+        // Act
+        bool result = unit1 > unit2;
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void GreaterThanOrEqualToOperator_Should_Return_True()
+    {
+        // Arrange
+        Unit unit1 = Unit.Default;
+        Unit unit2 = Unit.Default;
+
+        // Act
+        bool result = unit1 >= unit2;
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void LessThanOperator_Should_Return_False()
+    {
+        // Arrange
+        Unit unit1 = Unit.Default;
+        Unit unit2 = Unit.Default;
+
+        // Act
+        bool result = unit1 < unit2;
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void LessThanOrEqualToOperator_Should_Return_True()
+    {
+        // Arrange
+        Unit unit1 = Unit.Default;
+        Unit unit2 = Unit.Default;
+
+        // Act
+        bool result = unit1 <= unit2;
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void AdditionOperator_Should_Return_DefaultUnit()
+    {
+        // Arrange
+        Unit unit1 = Unit.Default;
+        Unit unit2 = Unit.Default;
+
+        // Act
+        Unit result = unit1 + unit2;
+
+        // Assert
+        result.Should().Be(Unit.Default);
+    }
+
+    [Fact]
+    public void Return_Value_Should_Return_ProvidedValue()
+    {
+        // Arrange
+        Unit unit = Unit.Default;
+        int expectedValue = 42;
+
+        // Act
+        int result = unit.Return(expectedValue);
+
+        // Assert
+        result.Should().Be(expectedValue);
+    }
+
+    [Fact]
+    public void Return_Func_Should_Execute_FunctionAndReturn_Result()
+    {
+        // Arrange
+        Unit unit = Unit.Default;
+        int expectedValue = 42;
+        Func<int> func = () => expectedValue;
+
+        // Act
+        int result = unit.Return(func);
+
+        // Assert
+        result.Should().Be(expectedValue);
+    }
+
+    [Fact]
+    public void ImplicitOperator_UnitToValueTuple_Should_ConvertCorrectly()
+    {
+        // Arrange
+        Unit unit = Unit.Default;
+
+        // Act
+        ValueTuple valueTuple = unit;
+
+        // Assert
+        valueTuple.Should().Be(ValueTuple.Create());
+    }
+
+    [Fact]
+    public void ImplicitOperator_ValueTupleToUnit_Should_ConvertCorrectly()
+    {
+        // Arrange
+        ValueTuple valueTuple = ValueTuple.Create();
+
+        // Act
+        Unit unit = valueTuple;
+
+        // Assert
+        unit.Should().Be(Unit.Default);
+    }
+}
---
+++ ToolBox/Common/Unit.cs
@@ -0,0 +1,57 @@
+﻿#nullable disable
+using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
+
+namespace ZeidLab.ToolBox.Common;
+
+/// <summary>
+/// A unit type is a type that allows only one value (and thus can hold no information)
+/// </summary>
+[Serializable]
+[StructLayout(LayoutKind.Sequential, Size = 1)]
+public readonly record struct Unit : IComparable<Unit>
+{
+    public static readonly Unit Default = new();
+
+    [Pure]
+    public override string ToString() => "()";
+
+    [Pure]
+    public static bool operator >(Unit lhs, Unit rhs) => false;
+
+    [Pure]
+    public static bool operator >=(Unit lhs, Unit rhs) => true;
+
+    [Pure]
+    public static bool operator <(Unit lhs, Unit rhs) => false;
+
+    [Pure]
+    public static bool operator <=(Unit lhs, Unit rhs) => true;
+
+    /// <summary>Provide an alternative value to unit</summary>
+    /// <typeparam name="T">Alternative value type</typeparam>
+    /// <param name="anything">Alternative value</param>
+    /// <returns>Alternative value</returns>
+    [Pure]
+    public T Return<T>(T anything) => anything;
+
+    /// <summary>Provide an alternative value to unit</summary>
+    /// <typeparam name="T">Alternative value type</typeparam>
+    /// <param name="anything">Alternative value</param>
+    /// <returns>Alternative value</returns>
+    [Pure]
+    public T Return<T>(Func<T> anything) => anything();
+
+    /// <summary>Always equal</summary>
+    [Pure]
+    public int CompareTo(Unit other) => 0;
+
+    [Pure]
+    public static Unit operator +(Unit a, Unit b) => Unit.Default;
+
+    [Pure]
+    public static implicit operator ValueTuple(Unit _) => new();
+
+    [Pure]
+    public static implicit operator Unit(ValueTuple _) => new();
+}
