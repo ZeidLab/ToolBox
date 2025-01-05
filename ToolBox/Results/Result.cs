240fe969b5f4be9d6c0293bb2d9273e63e554575
Zeid Youssefzadeh <zeidlabco@gmail.com>
2025-01-05 21:53:12 +0330

    Introducing new Type named Result with its few extensions

---
+++ ToolBox/Results/Result.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using ZeidLab.ToolBox.Common;
+
+namespace ZeidLab.ToolBox.Results;
+
+public readonly record struct Result<TValue>
+{
+    internal readonly TValue Value;
+    internal readonly Error? Error;
+
+    /// <summary>
+    /// Gets a value indicating whether the operation was successful.
+    /// </summary>
+    public readonly bool IsSuccess;
+
+    /// <summary>
+    /// Gets a value indicating whether the Value is default.
+    /// </summary>
+    public readonly bool IsDefault;
+
+    /// <summary>
+    /// Gets a value indicating whether the operation failed.
+    /// </summary>
+    [Pure]
+    public bool IsFailure => !IsSuccess;
+
+    private Result(bool isSuccess, TValue value, Error? error)
+    {
+        switch (isSuccess)
+        {
+            case true when (error is not null || value is null):
+                throw new ArgumentException("Value cannot be null and error should be null when IsSuccess is true.");
+            case false when (error is null || value is not null):
+                throw new ArgumentException(
+                    "Value should be null and error should not be null when IsSuccess is false.");
+        }
+        Value = value;
+        Error = error;
+        IsSuccess = isSuccess;
+        IsDefault = Value.IsDefault();
+    }
+
+    [Pure]
+    public static implicit operator Result<TValue>(TValue value) => Success(value);
+
+    [Pure]
+    public static implicit operator Result<TValue>(Error error) => Failure(error);
+
+    [Pure]
+    public static implicit operator Result<TValue>(Exception error) => Failure(error);
+
+    /// <summary>
+    /// Returns a successful result with a value.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value.</typeparam>
+    /// <param name="value">The value to include in the result.</param>
+    /// <returns>A successful <see cref="Result{TValue}"/>.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Result<TValue> Success(TValue value)
+    {
+        ArgumentNullException.ThrowIfNull(value, nameof(value));
+        return new Result<TValue>(true, value, null);
+    }
+
+    /// <summary>
+    /// Returns a failed result with the specified error.
+    /// </summary>
+    /// <param name="error">The error to include in the result.</param>
+    /// <returns>A failed <see cref="Result{TValue}"/>.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Result<TValue> Failure(Error error) => new(false, default!, error);
+}
---
+++ ToolBox/Results/ResultExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using ZeidLab.ToolBox.Options;
+
+namespace ZeidLab.ToolBox.Results;
+
+public static class ResultExtensions
+{
+    /// <summary>
+    /// Converts the provided object to a successful Result instance.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the object.</typeparam>
+    /// <param name="self">The object to convert.</param>
+    /// <returns>A successful Result instance containing the provided object.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Result<TIn> ToSuccess<TIn>(this TIn self)
+        => Result<TIn>.Success(self);
+
+    /// <summary>
+    /// Converts the provided error to a failed Result instance.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the value that would have been contained in the successful result.</typeparam>
+    /// <param name="self">The error to convert.</param>
+    /// <returns>A failed Result instance containing the provided error.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Result<TIn> ToFailure<TIn>(this Error self)
+        => Result<TIn>.Failure(self);
+
+    /// <summary>
+    /// Converts the provided exception to a failed Result instance.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the value that would have been contained in the successful result.</typeparam>
+    /// <param name="self">The exception to convert.</param>
+    /// <returns>A failed Result instance containing the provided exception as an error.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Result<TIn> ToFailure<TIn>(this Exception self)
+        => Result<TIn>.Failure(Error.New(self));
+    
+    /// <summary>
+    /// Converts a Result instance to a Maybe instance.
+    /// </summary>
+    /// <remarks>
+    /// If the result is successful, the value of the result is returned as a <see cref="Maybe{TIn}.Some"/> instance.
+    /// If the result is failed, a <see cref="Maybe{TIn}.None"/> instance is returned.
+    /// </remarks>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Maybe<TIn> ToMaybe<TIn>(this Result<TIn> self)
+        => self.IsSuccess ? Maybe<TIn>.Some(self.Value!) 
+            : Maybe<TIn>.None();
+}
