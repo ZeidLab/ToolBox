f5399909e9b228fdaab34ebad18a457cb3f3892b
Zeid Youssefzadeh <zeidlabco@gmail.com>
2025-01-08 13:39:25 +0330

    Adding binding extension methods

---
+++ ToolBox.Test.Units/Results/ResultExtensionsBindTests.cs
@@ -0,0 +1,187 @@
+﻿using FluentAssertions;
+using ZeidLab.ToolBox.Results;
+
+namespace ZeidLab.ToolBox.Test.Units.Results;
+
+public class ResultExtensionsBindTests
+{
+    private static Result<TValue> CreateSuccessResult<TValue>(TValue value) => Result<TValue>.Success(value);
+    private static Result<TValue> CreateFailureResult<TValue>(Error error) => Result<TValue>.Failure(error);
+    private static Error CreateError(string message = "Error message") => Error.New(message);
+
+    [Fact]
+    public void Bind_WithSuccessResult_ShouldApplyFunctionAndReturnNewResult()
+    {
+        // Arrange
+        var successResult = CreateSuccessResult(42);
+        Func<int, Result<string>> func = value => CreateSuccessResult(value.ToString());
+
+        // Act
+        var result = successResult.Bind(func);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be("42");
+    }
+
+    [Fact]
+    public void Bind_WithFailureResult_ShouldReturnFailureResultWithoutApplyingFunction()
+    {
+        // Arrange
+        var error = CreateError();
+        var failureResult = CreateFailureResult<int>(error);
+        Func<int, Result<string>> func = value => CreateSuccessResult(value.ToString());
+
+        // Act
+        var result = failureResult.Bind(func);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(error);
+    }
+
+    [Fact]
+    public void Bind_WithTryDelegate_ShouldApplyFunctionAndReturnNewResult()
+    {
+        // Arrange
+        var successResult = CreateSuccessResult(42);
+        Try<int> tryDelegate = () => successResult;
+        Func<int, Result<string>> func = value => CreateSuccessResult(value.ToString());
+
+        // Act
+        var result = tryDelegate.Bind(func);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be("42");
+    }
+
+    [Fact]
+    public void BindAsync_WithSuccessResult_ShouldApplyAsyncFunctionAndReturnNewResult()
+    {
+        // Arrange
+        var successResult = CreateSuccessResult(42);
+        Func<int, Task<Result<string>>> func = value => Task.FromResult(CreateSuccessResult(value.ToString()));
+
+        // Act
+        var resultTask = successResult.BindAsync(func);
+        resultTask.Wait();
+        var result = resultTask.Result;
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be("42");
+    }
+
+    [Fact]
+    public void BindAsync_WithFailureResult_ShouldReturnFailureResultWithoutApplyingAsyncFunction()
+    {
+        // Arrange
+        var error = CreateError();
+        var failureResult = CreateFailureResult<int>(error);
+        Func<int, Task<Result<string>>> func = value => Task.FromResult(CreateSuccessResult(value.ToString()));
+
+        // Act
+        var resultTask = failureResult.BindAsync(func);
+        resultTask.Wait();
+        var result = resultTask.Result;
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(error);
+    }
+
+    [Fact]
+    public async Task BindAsync_WithTaskSuccessResult_ShouldApplyFunctionAndReturnNewResult()
+    {
+        // Arrange
+        var successResult = Task.FromResult(CreateSuccessResult(42));
+        Func<int, Result<string>> func = value => CreateSuccessResult(value.ToString());
+
+        // Act
+        var result = await successResult.BindAsync(func);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be("42");
+    }
+
+    [Fact]
+    public async Task BindAsync_WithTaskFailureResult_ShouldReturnFailureResultWithoutApplyingFunction()
+    {
+        // Arrange
+        var error = CreateError();
+        var failureResult = Task.FromResult(CreateFailureResult<int>(error));
+        Func<int, Result<string>> func = value => CreateSuccessResult(value.ToString());
+
+        // Act
+        var result = await failureResult.BindAsync(func);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(error);
+    }
+
+    [Fact]
+    public async Task BindAsync_WithTaskSuccessResultAndAsyncFunction_ShouldApplyAsyncFunctionAndReturnNewResult()
+    {
+        // Arrange
+        var successResult = Task.FromResult(CreateSuccessResult(42));
+        Func<int, Task<Result<string>>> func = value => Task.FromResult(CreateSuccessResult(value.ToString()));
+
+        // Act
+        var result = await successResult.BindAsync(func);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be("42");
+    }
+
+    [Fact]
+    public async Task BindAsync_WithTaskFailureResultAndAsyncFunction_ShouldReturnFailureResultWithoutApplyingAsyncFunction()
+    {
+        // Arrange
+        var error = CreateError();
+        var failureResult = Task.FromResult(CreateFailureResult<int>(error));
+        Func<int, Task<Result<string>>> func = value => Task.FromResult(CreateSuccessResult(value.ToString()));
+
+        // Act
+        var result = await failureResult.BindAsync(func);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(error);
+    }
+
+    [Fact]
+    public async Task BindAsync_WithTryAsyncDelegate_ShouldApplyFunctionAndReturnNewResult()
+    {
+        // Arrange
+        var successResult = CreateSuccessResult(42);
+        TryAsync<int> tryAsyncDelegate = () => Task.FromResult(successResult);
+        Func<int, Result<string>> func = value => CreateSuccessResult(value.ToString());
+
+        // Act
+        var result = await tryAsyncDelegate.BindAsync(func);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be("42");
+    }
+
+    [Fact]
+    public async Task BindAsync_WithTryAsyncDelegateAndAsyncFunction_ShouldApplyAsyncFunctionAndReturnNewResult()
+    {
+        // Arrange
+        var successResult = CreateSuccessResult(42);
+        TryAsync<int> tryAsyncDelegate = () => Task.FromResult(successResult);
+        Func<int, Task<Result<string>>> func = value => Task.FromResult(CreateSuccessResult(value.ToString()));
+
+        // Act
+        var result = await tryAsyncDelegate.BindAsync(func);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be("42");
+    }
+}
---
+++ ToolBox/Results/ResultExtensionsBind.cs
@@ -0,0 +1,130 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using ZeidLab.ToolBox.Common;
+
+namespace ZeidLab.ToolBox.Results;
+
+/// <summary>
+/// Provides extension methods for binding operations on <see cref="Result{TValue}"/> objects.
+/// </summary>
+public static class ResultExtensionsBind
+{
+    #region Bind
+
+    /// <summary>
+    /// Binds the value of a successful result to a new result by applying the specified function.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the value of the original result.</typeparam>
+    /// <typeparam name="TOut">The type of the value of the new result.</typeparam>
+    /// <param name="self">The original result.</param>
+    /// <param name="func">The function to apply to the value of the original result.</param>
+    /// <returns>The result of applying the specified function to the value of the original result.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Result<TOut> Bind<TIn, TOut>(this Result<TIn> self,
+        Func<TIn, Result<TOut>> func) 
+        => self.IsSuccess 
+            ? func(self.Value) 
+            : Result<TOut>
+                .Failure(self.Error.GetValueOrDefault());
+
+    /// <summary>
+    /// Binds the value of a successful <see cref="Try{TIn}"/> to a new result by applying the specified function.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the value of the original <see cref="Try{TIn}"/>.</typeparam>
+    /// <typeparam name="TOut">The type of the value of the new result.</typeparam>
+    /// <param name="self">The original <see cref="Try{TIn}"/>.</param>
+    /// <param name="func">The function to apply to the value of the original <see cref="Try{TIn}"/>.</param>
+    /// <returns>The result of applying the specified function to the value of the original <see cref="Try{TIn}"/>.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Result<TOut> Bind<TIn, TOut>(this Try<TIn> self,
+        Func<TIn, Result<TOut>> func)
+        => self.Try().Bind(func);
+
+    #endregion
+
+    #region BindAsync
+
+    /// <summary>
+    /// Binds the value of a successful result to a new result by applying the specified asynchronous function.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the value of the original result.</typeparam>
+    /// <typeparam name="TOut">The type of the value of the new result.</typeparam>
+    /// <param name="self">The original result.</param>
+    /// <param name="func">The asynchronous function to apply to the value of the original result.</param>
+    /// <returns>The result of applying the specified asynchronous function to the value of the original result.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Task<Result<TOut>> BindAsync<TIn, TOut>(this Result<TIn> self,
+        Func<TIn, Task<Result<TOut>>> func)
+    => self.IsSuccess
+            ? func(self.Value)
+            : Result<TOut>
+                .Failure(self.Error.GetValueOrDefault())
+                .AsTask();
+    
+
+
+    /// <summary>
+    /// Binds the value of a successful result to a new result by applying the specified function.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the value of the original result.</typeparam>
+    /// <typeparam name="TOut">The type of the value of the new result.</typeparam>
+    /// <param name="self">The original result.</param>
+    /// <param name="func">The function to apply to the value of the original result.</param>
+    /// <returns>The result of applying the specified function to the value of the original result.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static async Task<Result<TOut>> BindAsync<TIn, TOut>(this Task<Result<TIn>> self,
+        Func<TIn, Result<TOut>> func)
+        => (await self.ConfigureAwait(false)).Bind(func);
+
+    /// <summary>
+    /// Asynchronously binds the value of a successful result to a new result by applying the specified asynchronous function.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the value of the original result.</typeparam>
+    /// <typeparam name="TOut">The type of the value of the new result.</typeparam>
+    /// <param name="self">A task that represents the asynchronous operation of obtaining the original result.</param>
+    /// <param name="func">The asynchronous function to apply to the value of the original result.</param>
+    /// <returns>A task representing the asynchronous operation, containing the result of applying the specified asynchronous function to the value of the original result.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static async Task<Result<TOut>> BindAsync<TIn, TOut>(this Task<Result<TIn>> self,
+        Func<TIn, Task<Result<TOut>>> func)
+    {
+        var result = await self;
+        return result.IsSuccess
+            ? await func(result.Value).ConfigureAwait(false)
+            : Result<TOut>.Failure(result.Error.GetValueOrDefault());
+    }
+
+    /// <summary>
+    /// Asynchronously binds the value of a successful result to a new result by applying the specified function.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the value of the original result.</typeparam>
+    /// <typeparam name="TOut">The type of the value of the new result.</typeparam>
+    /// <param name="self">A try-async that represents the asynchronous operation of obtaining the original result.</param>
+    /// <param name="func">The function to apply to the value of the original result.</param>
+    /// <returns>A task representing the asynchronous operation, containing the result of applying the specified function to the value of the original result.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static async Task<Result<TOut>> BindAsync<TIn, TOut>(this TryAsync<TIn> self,
+        Func<TIn, Result<TOut>> func)
+        => (await self.Try()).Bind(func);
+
+    /// <summary>
+    /// Asynchronously binds the value of a successful result to a new result by applying the specified asynchronous function.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the value of the original result.</typeparam>
+    /// <typeparam name="TOut">The type of the value of the new result.</typeparam>
+    /// <param name="self">A try-async that represents the asynchronous operation of obtaining the original result.</param>
+    /// <param name="func">The asynchronous function to apply to the value of the original result.</param>
+    /// <returns>A task representing the asynchronous operation, containing the result of applying the specified asynchronous function to the value of the original result.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Task<Result<TOut>> BindAsync<TIn, TOut>(this TryAsync<TIn> self, Func<TIn, Task<Result<TOut>>> func)
+        => self.Try().BindAsync(func);
+
+    #endregion
+}
