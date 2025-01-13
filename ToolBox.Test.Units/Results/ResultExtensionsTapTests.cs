a05dc734c15941a803cba280b15dea1947900342
Zeid Youssefzadeh <zeidlabco@gmail.com>
2025-01-13 13:29:56 +0330

    Adding Tap method in its Test to library

---
+++ ToolBox.Test.Units/Results/ResultExtensionsTapTests.cs
@@ -0,0 +1,196 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using ZeidLab.ToolBox.Results;
+
+namespace ZeidLab.ToolBox.Test.Units.Results
+{
+    public class ResultExtensionsTapTests
+    {
+        // Helper methods
+        private static Result<T> CreateSuccessResult<T>(T value) => Result<T>.Success(value);
+        private static Result<T> CreateFailureResult<T>(ResultError error) => Result<T>.Failure(error);
+
+        // Tests for Tap (synchronous)
+        [Fact]
+        public void Tap_ShouldExecuteAction_WhenResultIsSuccess()
+        {
+            // Arrange
+            var result = CreateSuccessResult(42);
+            var action = Substitute.For<Action<int>>();
+
+            // Act
+            var returnedResult = result.Tap(action);
+
+            // Assert
+            returnedResult.Should().Be(result);
+            action.Received(1).Invoke(42);
+        }
+
+        [Fact]
+        public void Tap_ShouldNotExecuteAction_WhenResultIsFailure()
+        {
+            // Arrange
+            var result = CreateFailureResult<int>(ResultError.New("Error"));
+            var action = Substitute.For<Action<int>>();
+
+            // Act
+            var returnedResult = result.Tap(action);
+
+            // Assert
+            returnedResult.Should().Be(result);
+            action.DidNotReceiveWithAnyArgs().Invoke(default);
+        }
+
+
+        [Fact]
+        public void TapAsync_WithAction_ShouldThrowArgumentNullException_WhenActionIsNull()
+        {
+            // Arrange
+            var result = CreateSuccessResult(42);
+
+            // Act
+            Func<Task> act = () => result.TapAsync(null);
+
+            // Assert
+            act.Should().ThrowAsync<ArgumentNullException>();
+        }
+
+        // Tests for TapAsync (with Func)
+        [Fact]
+        public async Task TapAsync_WithFunc_ShouldExecuteFunc_WhenResultIsSuccess()
+        {
+            // Arrange
+            var result = CreateSuccessResult(42);
+            var func = Substitute.For<Func<int, Task>>();
+
+            // Act
+            var returnedResult = await result.TapAsync(func);
+
+            // Assert
+            returnedResult.Should().Be(result);
+            await func.Received(1).Invoke(42);
+        }
+
+        [Fact]
+        public async Task TapAsync_WithFunc_ShouldNotExecuteFunc_WhenResultIsFailure()
+        {
+            // Arrange
+            var result = CreateFailureResult<int>(ResultError.New("Error"));
+            var func = Substitute.For<Func<int, Task>>();
+
+            // Act
+            var returnedResult = await result.TapAsync(func);
+
+            // Assert
+            returnedResult.Should().Be(result);
+            await func.DidNotReceiveWithAnyArgs().Invoke(default);
+        }
+
+        [Fact]
+        public void TapAsync_WithFunc_ShouldThrowArgumentNullException_WhenFuncIsNull()
+        {
+            // Arrange
+            var result = CreateSuccessResult(42);
+
+            // Act
+            Func<Task> act = () => result.TapAsync(null);
+
+            // Assert
+            act.Should().ThrowAsync<ArgumentNullException>();
+        }
+
+        // Tests for TapAsync with Try
+        [Fact]
+        public async Task TapAsync_WithTry_ShouldExecuteFunc_WhenTryIsSuccess()
+        {
+            // Arrange
+            var result = CreateSuccessResult(42);
+            var tryResult = new Try<int>(() => result);
+            var func = Substitute.For<Func<int, Task>>();
+
+            // Act
+            var returnedResult = await tryResult.TapAsync(func);
+
+            // Assert
+            returnedResult.Should().Be(result);
+            await func.Received(1).Invoke(42);
+        }
+
+        [Fact]
+        public async Task TapAsync_WithTry_ShouldNotExecuteFunc_WhenTryIsFailure()
+        {
+            // Arrange
+            var result = CreateFailureResult<int>(ResultError.New("Error"));
+            var tryResult = new Try<int>(() => result);
+            var func = Substitute.For<Func<int, Task>>();
+
+            // Act
+            var returnedResult = await tryResult.TapAsync(func);
+
+            // Assert
+            returnedResult.Should().Be(result);
+            await func.DidNotReceiveWithAnyArgs().Invoke(default);
+        }
+
+        [Fact]
+        public void TapAsync_WithTry_ShouldThrowArgumentNullException_WhenFuncIsNull()
+        {
+            // Arrange
+            var result = CreateSuccessResult(42);
+            var tryResult = new Try<int>(() => result);
+
+            // Act
+            Func<Task> act = () => tryResult.TapAsync(null);
+
+            // Assert
+            act.Should().ThrowAsync<ArgumentNullException>();
+        }
+
+        // Tests for TapAsync with TryAsync
+        [Fact]
+        public async Task TapAsync_WithTryAsync_ShouldExecuteFunc_WhenTryAsyncIsSuccess()
+        {
+            // Arrange
+            var result = CreateSuccessResult(42);
+            var tryAsyncResult = new TryAsync<int>(() => Task.FromResult(result));
+            var func = Substitute.For<Func<int, Task>>();
+
+            // Act
+            var returnedResult = await tryAsyncResult.TapAsync(func);
+
+            // Assert
+            returnedResult.Should().Be(result);
+            await func.Received(1).Invoke(42);
+        }
+
+        [Fact]
+        public async Task TapAsync_WithTryAsync_ShouldNotExecuteFunc_WhenTryAsyncIsFailure()
+        {
+            // Arrange
+            var result = CreateFailureResult<int>(ResultError.New("Error"));
+            var tryAsyncResult = new TryAsync<int>(() => Task.FromResult(result));
+            var func = Substitute.For<Func<int, Task>>();
+
+            // Act
+            var returnedResult = await tryAsyncResult.TapAsync(func);
+
+            // Assert
+            returnedResult.Should().Be(result);
+            await func.DidNotReceiveWithAnyArgs().Invoke(default);
+        }
+
+        [Fact]
+        public void TapAsync_WithTryAsync_ShouldThrowArgumentNullException_WhenFuncIsNull()
+        {
+            // Arrange
+            var result = CreateSuccessResult(42);
+            var tryAsyncResult = new TryAsync<int>(() => Task.FromResult(result));
+
+            // Act
+            Func<Task> act = () => tryAsyncResult.TapAsync(null);
+
+            // Assert
+            act.Should().ThrowAsync<ArgumentNullException>();
+        }
+    }
+}
---
+++ ToolBox/Results/ResultExtensionsTap.cs
@@ -0,0 +1,117 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+
+namespace ZeidLab.ToolBox.Results;
+
+/// <summary>
+/// provides extension methods for tapping operations on <see cref="Result{TValue}"/> objects.
+/// it executes a side effect if the result is successful without interrupting the flow of the process.
+/// </summary>
+[SuppressMessage("Design", "CA1062:Validate arguments of public methods")]
+public static class ResultExtensionsTap
+{
+    /// <summary>
+    /// Executes a side effect if the result is successful without interrupting the flow of the process.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the result value.</typeparam>
+    /// <param name="self">The result to tap.</param>
+    /// <param name="action">The side effect to execute.</param>
+    /// <returns>The original result.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Result<TIn> Tap<TIn>(this Result<TIn> self, Action<TIn> action)
+    {
+        if (self.IsSuccess) action(self.Value);
+        return self;
+    }
+
+    /// <summary>
+    /// Executes a side effect on the value of a successful <see cref="Try{TIn}"/> without interrupting the flow of the process.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the result value.</typeparam>
+    /// <param name="self">The Try instance to tap.</param>
+    /// <param name="action">The side effect to execute on the value if successful.</param>
+    /// <returns>The original result after executing the side effect.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Result<TIn> Tap<TIn>(this Try<TIn> self, Action<TIn> action)
+        => self.Try().Tap(action);
+
+    /// <summary>
+    /// Asynchronously executes a side effect if the result is successful without interrupting the flow of the process.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the result value.</typeparam>
+    /// <param name="self">The task representing the result to tap.</param>
+    /// <param name="action">The side effect to execute.</param>
+    /// <returns>A task representing the asynchronous operation, containing the original result.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static async Task<Result<TIn>> TapAsync<TIn>(this Task<Result<TIn>> self, Action<TIn> action)
+    {
+        var result = await self.ConfigureAwait(false);
+        if (result.IsSuccess) action(result.Value);
+
+        return result;
+    }
+
+    /// <summary>
+    /// Asynchronously executes a side effect on the value of a successful <see cref="TryAsync{TIn}"/> without interrupting the flow of the process.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the result value.</typeparam>
+    /// <param name="self">The asynchronous <see cref="TryAsync{TIn}"/> to tap.</param>
+    /// <param name="action">The side effect to execute on the value if successful.</param>
+    /// <returns>A task representing the asynchronous operation, containing the original result.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Task<Result<TIn>> TapAsync<TIn>(this TryAsync<TIn> self, Action<TIn> action)
+        => self.TryAsync().TapAsync(action);
+
+
+    /// <summary>
+    /// Asynchronously executes a side effect if the result is successful without interrupting the flow of the process.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the result value.</typeparam>
+    /// <param name="self">The result to tap.</param>
+    /// <param name="func">The asynchronous side effect to execute.</param>
+    /// <returns>The original result.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static async Task<Result<TIn>> TapAsync<TIn>(this Result<TIn> self, Func<TIn, Task> func)
+    {
+        if (self.IsSuccess) await func(self.Value).ConfigureAwait(false);
+        return self;
+    }
+
+    /// <summary>
+    /// Asynchronously executes a side effect on the value of a successful <see cref="Try{TIn}"/> without interrupting the flow of the process.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the result value.</typeparam>
+    /// <param name="self">The Try instance to tap.</param>
+    /// <param name="func">The asynchronous side effect to execute on the value if successful.</param>
+    /// <returns>The original result after executing the side effect.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static async Task<Result<TIn>> TapAsync<TIn>(this Try<TIn> self, Func<TIn, Task> func)
+    {
+        var result = self.Try();
+        if (result.IsSuccess) await func(result.Value).ConfigureAwait(false);
+        return result;
+    }
+
+    /// <summary>
+    /// Asynchronously executes a side effect on the value of a successful <see cref="TryAsync{TIn}"/> without interrupting the flow of the process.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the result value.</typeparam>
+    /// <param name="self">The asynchronous <see cref="TryAsync{TIn}"/> to tap.</param>
+    /// <param name="func">The asynchronous side effect to execute on the value if successful.</param>
+    /// <returns>A task representing the asynchronous operation, containing the original result.</returns>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static async Task<Result<TIn>> TapAsync<TIn>(this TryAsync<TIn> self, Func<TIn, Task> func)
+    {
+        var result = await self.TryAsync().ConfigureAwait(false);
+        if (result.IsSuccess) await func(result.Value).ConfigureAwait(false);
+        return result;
+    }
+}
