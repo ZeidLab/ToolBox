56599beb72af8e264bc3431781bae7e997f2f670
Zeid Youssefzadeh <zeidlabco@gmail.com>
2025-02-18 21:35:51 +0330

    Add example project with result binding and error handling demonstrations and an DocFragments.xml for improving and minifying the documentation

---
+++ ToolBox.ExampleProject/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ZeidLab.ToolBox.ExampleProject.ResultExamples;
+
+Console.WriteLine("Hello, World!");
+
+await BindExamples.RunAsync();
